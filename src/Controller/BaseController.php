ea75ec67e43ecce45259b850fef36120a7ddb04b
Ashad Khan <asad60khan@gmail.com>
2019-06-08 01:10:04 +0200

    3-service movie API

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,16 +2,29 @@
 
 namespace App\Controller;
 
+use App\Services\MovieAPI;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BaseController extends AbstractController
 {
+    private $movieAPI;
+
+    public function __construct(MovieAPI $movieAPI)
+    {
+        $this->movieAPI = $movieAPI;
+    }
+
+
     /**
      * @Route("/", name="base")
      */
     public function index()
     {
+        $movies = $this->movieAPI->getPopularMovies();
+        dump($movies);
+        die;
+
         return $this->render('base/index.html.twig', [
             'controller_name' => 'BaseController',
         ]);
---
+++ src/Services/MovieAPI.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Services;
+
+
+use Tmdb\Repository\MovieRepository;
+
+class MovieAPI
+{
+    private $client;
+    private $repository;
+
+    public function __construct($client)
+    {
+        $this->client = $client;
+        $this->repository = new MovieRepository($client);
+    }
+
+    public function getLatestMovies($params = []){
+        return $this->repository->getLatest($params);
+    }
+
+    public function getPopularMovies($params = []){
+        return $this->repository->getPopular($params);
+    }
+
+    public function getNowPlayingMovies($params = []){
+        return $this->repository->getNowPlaying($params);
+    }
+
+    public function getUpcomingMovies($params = []){
+        return $this->repository->getUpcoming($params);
+    }
+
+    public function getTopRatedMovies($params = []){
+        return $this->repository->getTopRated($params);
+    }
+}
