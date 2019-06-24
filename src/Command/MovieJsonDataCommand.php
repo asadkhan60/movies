529f7fff437f802e222e0851ad72c437047f0f93
Ashad Khan <asad60khan@gmail.com>
2019-06-25 00:38:08 +0200

    16-command generate files

--- src/Command/MovieJsonDataCommand.php
+++ src/Command/MovieJsonDataCommand.php
@@ -4,18 +4,51 @@
 namespace App\Command;
 
 
+use App\Services\MovieAPI;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 class MovieJsonDataCommand extends Command
 {
     protected static $defaultName = 'movie:generate-files';
+
+    private $movieApi;
+
+    public function __construct(MovieAPI $movieAPI)
+    {
+        $this->movieApi = $movieAPI;
+
+        parent::__construct();
+    }
 
     protected function configure()
     {
         $this
             ->setDescription('Generate json files from movies API')
             ->setHelp('This command allows you to create json files 
-            (now movies, popular movies, upcoming movies and top rated movies) which content all movies data')
+             for now movies, popular movies, upcoming movies and top rated movies')
+            ->addArgument('now', InputArgument::OPTIONAL, 'Now movies')
+            ->addArgument('popular', InputArgument::OPTIONAL, 'Popular movies')
+            ->addArgument('upcoming', InputArgument::OPTIONAL, 'Upcoming movies')
+            ->addArgument('toprated', InputArgument::OPTIONAL, 'Top rated movies')
         ;
+    }
+
+
+    private function nowMoviesData(){
+        $page = 1;
+        $nowMovies = $this->movieApi->getNowPlayingMovies(['page' => $page]);
+
+        var_dump($nowMovies);
+        die;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->nowMoviesData();
+
+        $output->writeln('User successfully generated!');
     }
 }
