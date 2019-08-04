3478a6add2d90f0497675c581efcab34e36fbea7
Fahad Khan <asad60khan@gmail.com>
2019-08-04 13:55:27 +0200

    21-serializer

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -6,6 +6,7 @@ use App\Document\Movie;
 use App\Enum\MovieEnum;
 use App\Repository\MovieRepository;
 use App\Services\MovieHelper;
+use App\Services\Serializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +14,13 @@ class BaseController extends AbstractController
 {
     private $movieHelper;
     private $movieRepository;
+    private $serializer;
 
-    public function __construct(MovieHelper $movieHelper, MovieRepository $movieRepository)
+    public function __construct(MovieHelper $movieHelper, MovieRepository $movieRepository, Serializer $serializer)
     {
         $this->movieHelper = $movieHelper;
         $this->movieRepository = $movieRepository;
+        $this->serializer = $serializer;
     }
 
 
@@ -32,8 +35,8 @@ class BaseController extends AbstractController
         $topRatedMovies = $this->movieRepository->getTopRatedMovies(8);
         $recentMovies = $this->movieRepository->getRecentMovies(8);
 
-        dump($nowPlayingMovies);
-        die;
+        /*$nowMovies = $this->serializer->serialize($nowPlayingMovies, 'json');
+        $newNowMovies = $this->serializer->deserialize($nowMovies, \App\Entity\Movie::class . '[]', 'json');*/
 
         return $this->render('base/index.html.twig', [
             'nowPlayingMovies' => $nowPlayingMovies,
