792f94c154218059c125b4b83da7b0415244a0a6
Fahad Khan <asad60khan@gmail.com>
2019-08-13 01:51:01 +0200

    28-redis installation

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Tmdb\SymfonyBundle\TmdbSymfonyBundle::class => ['all' => true],
-    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true]
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
 ];
